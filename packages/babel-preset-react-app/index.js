cc59aa2d2bc397459aca68bdcf9ea8c25aec089a
duduxiong <xiang_linzong@163.com>
2019-01-23 17:58:31 +0800

    docs: initialized project

---
+++ packages/babel-preset-react-app/index.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+'use strict';
+
+const create = require('./create');
+
+module.exports = function(api, opts) {
+//  基于不同的开发环境
+  const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+  return create(api, opts, env);
+};
